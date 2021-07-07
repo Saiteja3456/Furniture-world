bc82d241f8f3af671179ad5b9ac9c911b8d0ba5a
Saiteja3456 <teja5d3@sasi.ac.in>
2021-07-07 21:10:15 +0530

    cart added

---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: 'Ohrensessel Josslyn',
+    price: 4999.9,
+    currency: '₹',
+    image: 'images/01.jpg',
+  },
+  {
+    id: 2,
+    name: 'Sessel Sofie',
+    price: 2499.99,
+    currency: '₹',
+    image: 'images/02.jpg',
+  },
+  {
+    id: 4,
+    name: 'Schlafsessel Rovigo',
+    price: 2399.99,
+    currency: '₹',
+    image: 'images/04.jpg',
+  },
+  {
+    id: 6,
+    name: 'Sessel Little',
+    price: 2099.99,
+    currency: '₹',
+    image: 'images/06.jpg',
+  },
+  {
+    id: 5,
+    name: 'Sessel Peacock',
+    price: 5999.99,
+    currency: '₹',
+    image: 'images/05.jpg',
+  },
+  {
+    id: 3,
+    name: 'Sessel Anna',
+    price: 2499.99,
+    currency: '₹',
+    image: 'images/03.jpg',
+  }
+];
+
+export default data;
